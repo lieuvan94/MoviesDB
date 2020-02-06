963853795cff250b8924e44c284845aa440aadf8
ThinhNP <v.thinhnp5@vinid.net>
2020-02-07 00:07:23 +0700

    feuture/implement_home_screen_#2(use case support offline first)

---
+++ app/src/main/java/net/vinid/moviedb/data/local/entity/MovieEntity.kt
@@ -0,0 +1,27 @@
+package net.vinid.moviedb.data.local.entity
+
+import io.realm.RealmList
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class MovieEntity(
+    @PrimaryKey var id: Int = 0,
+    var posterPath: String? = null,
+    var adult: Boolean = false,
+    var overview: String? = null,
+    var releaseDate: String? = null,
+    var genreIds: RealmList<Int>? = RealmList(),
+    var originalTitle: String? = null,
+    var originalLanguage: String? = null,
+    var title: String? = null,
+    var backdropPath: String? = null,
+    var popularity: Double? = null,
+    var voteCount: Int? = null,
+    var video: Boolean? = null,
+    var voteAverage: Double? = null,
+    var category: Int? = null,
+    var isLike: Boolean = false,
+    var page: Int? = null
+) : RealmObject()
+
+
