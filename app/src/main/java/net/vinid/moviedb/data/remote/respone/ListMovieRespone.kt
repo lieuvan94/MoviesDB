351b9d8f608ab4ca1a1564d1ca78d84218cd53ce
ThinhNP <v.thinhnp5@vinid.net>
2020-02-12 14:08:54 +0700

    Support offline first - Fix naming convention

--- app/src/main/java/net/vinid/moviedb/data/remote/respone/ListMovieRespone.kt
+++ app/src/main/java/net/vinid/moviedb/data/remote/respone/ListMovieRespone.kt
@@ -5,6 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class ListMovieRespone (
     @SerializedName("page") val page : Int,
     @SerializedName("results") val results : List<MovieRespone>,
-    @SerializedName("total_results") val total_results : Int,
-    @SerializedName("total_pages") val total_pages : Int
+    @SerializedName("total_results") val totalResults : Int,
+    @SerializedName("total_pages") val totalPages : Int
 )
