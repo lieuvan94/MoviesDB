88c7c32c544b04641d28a652e4104123dc89ca96
Nguyen Van Lieu (VinID) <v.lieunv2@vinid.net>
2020-02-14 12:18:02 +0700

    Call data to MoviesRepository

--- app/src/main/java/net/vinid/moviedb/data/local/dao/MovieDAO.kt
+++ app/src/main/java/net/vinid/moviedb/data/local/dao/MovieDAO.kt
@@ -7,4 +7,8 @@ interface MovieDAO {
     fun getMoviesByPage(page: Int): List<MovieEntity>
     fun searchMoviesByQuery(query :String): List<MovieEntity>
     fun saveListMovie(listMovie: List<MovieEntity>, category: String, page: Int)
+    fun deleteMovieByPageAndCategory(page: Int, category: String)
+    fun saveListMovie(listMovie: List<MovieEntity>)
+    fun getMoviesByPage(page: Int): List<MovieEntity>
+    fun searchMoviesByQuery(query :String): List<MovieEntity>
 }
