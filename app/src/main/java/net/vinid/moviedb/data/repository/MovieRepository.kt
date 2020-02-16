da013e385282e004ce01b6528d64a8619bfd6834
Nguyen Van Lieu (VinID) <v.lieunv2@vinid.net>
2020-02-16 16:59:56 +0700

    Implement search movies by name

--- app/src/main/java/net/vinid/moviedb/data/repository/MovieRepository.kt
+++ app/src/main/java/net/vinid/moviedb/data/repository/MovieRepository.kt
@@ -3,7 +3,6 @@ package net.vinid.moviedb.data.repository
 import io.reactivex.Observable
 import net.vinid.moviedb.data.local.entity.MovieEntity
 import net.vinid.moviedb.data.remote.api.Resource
-import net.vinid.moviedb.data.remote.respone.MovieResponse
 
 interface MovieRepository {
     fun getMovieByCategory(category: String, page: Int): Observable<Resource<List<MovieEntity>>>
