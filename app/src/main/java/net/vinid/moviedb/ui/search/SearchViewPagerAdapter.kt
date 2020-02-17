20bd6bc1fb3de52ec0007c8dc11b0ab2681129f7
Nguyen Van Lieu (VinID) <v.lieunv2@vinid.net>
2020-02-17 14:49:36 +0700

    #3 - Fix comment implement search movie by name

--- app/src/main/java/net/vinid/moviedb/ui/search/SearchViewPagerAdapter.kt
+++ app/src/main/java/net/vinid/moviedb/ui/search/SearchViewPagerAdapter.kt
@@ -3,6 +3,7 @@ package net.vinid.moviedb.ui.search
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import net.vinid.moviedb.R
 import net.vinid.moviedb.ui.search.view.SearchByMoviesNameFragment
 import net.vinid.moviedb.ui.search.view.SearchMoviesByCategoryFragment
 import net.vinid.moviedb.ui.search.view.SearchMoviesByGenresFragment
@@ -12,11 +13,10 @@ import net.vinid.moviedb.ui.search.view.SearchMoviesByGenresFragment
  */
 class SearchViewPagerAdapter(fragmentManager: FragmentManager): FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
-    companion object{
-        const val NUM_PAGER = 3
-        const val MOVIES_NAME = "Movies name"
-        const val CATEGORY = "Category"
-        const val GENRES = "Genres"
+    enum class PageTitle(val title :String) {
+        MOVIES_NAME("Movies name"),
+        CATEGORY("Category"),
+        GENRES("Genres")
     }
 
     override fun getItem(position: Int): Fragment = when (position) {
@@ -27,12 +27,13 @@ class SearchViewPagerAdapter(fragmentManager: FragmentManager): FragmentPagerAda
     }
 
     override fun getPageTitle(position: Int): CharSequence = when (position) {
-        0 -> MOVIES_NAME
-        1 -> CATEGORY
-        2 -> GENRES
+        0 -> PageTitle.MOVIES_NAME.title
+        1 -> PageTitle.CATEGORY.title
+        2 -> PageTitle.GENRES.title
         else -> ""
     }
 
     override fun getCount(): Int = NUM_PAGER
 
 }
+const val NUM_PAGER =3
--- app/src/main/java/net/vinid/moviedb/ui/search/view/SearchByMoviesNameFragment.kt
+++ app/src/main/java/net/vinid/moviedb/ui/search/view/SearchByMoviesNameFragment.kt
@@ -5,9 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import net.vinid.moviedb.MovieApplication
 import net.vinid.moviedb.R
+import net.vinid.moviedb.data.local.entity.MovieEntity
 import net.vinid.moviedb.databinding.FragmentSearchByMovieNameBinding
 import net.vinid.moviedb.ui.base.BaseFragment
+import net.vinid.moviedb.ui.home.MoviesAdapter
+import net.vinid.moviedb.ui.search.SearchMoviesViewModel
+import net.vinid.moviedb.util.AppUtils
 
 /**
  * Created by Nguyen Van Lieu on 2/1/2020.
@@ -15,6 +24,17 @@ import net.vinid.moviedb.ui.base.BaseFragment
 class SearchByMoviesNameFragment : BaseFragment() {
 
     private lateinit var dataBinding: FragmentSearchByMovieNameBinding
+    private lateinit var searchMoviesAdapter: MoviesAdapter
+
+    private val viewModelFactory = MovieApplication.injectViewModelFactory()
+
+    private val searchMoviesViewModel: SearchMoviesViewModel by lazy {
+        ViewModelProvider(this,viewModelFactory).get(SearchMoviesViewModel::class.java)
+    }
+
+    private val sharedViewModel: SearchSharedViewModel by activityViewModels()
+
+    private  lateinit var query: String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +44,38 @@ class SearchByMoviesNameFragment : BaseFragment() {
         dataBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_search_by_movie_name, container, false)
         dataBinding.lifecycleOwner = this
+        dataBinding.viewModel = searchMoviesViewModel
+
+        initView()
+        initViewModel()
 
         return dataBinding.root
+    }
+
+    private fun initView() {
+        searchMoviesAdapter = MoviesAdapter()
+        dataBinding.includedListMovieLayout.moviesRecyclerView.apply {
+            layoutManager = GridLayoutManager(context,AppUtils.COLUMN_RECYCLEVIEW)
+            adapter = this@SearchByMoviesNameFragment.searchMoviesAdapter
+        }
+    }
+
+    private fun initViewModel() {
+        sharedViewModel.keyword.observe(viewLifecycleOwner, Observer {
+            query = it
+            searchMoviesViewModel.requestSearchMovieByPage(query,AppUtils.QUERY_PAGE)
+        })
+        searchMoviesViewModel.searchMovies.observe(viewLifecycleOwner, Observer {
+            updateMoviesList(it)
+        })
+
+        // handle error
+        searchMoviesViewModel.errSearchMovies.observe(viewLifecycleOwner, Observer {
+            //Todo Show dialog display error
+        })
+    }
+    private fun updateMoviesList(movies: List<MovieEntity>) {
+        searchMoviesAdapter.setItems(movies)
     }
 
 }
